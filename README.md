4b03bbf4c602991fea90f5006cd7652d656a2f97
Sadegh Teymouri <sadeghte@gmail.com>
2021-03-15 11:34:51 +0330

    remove hard coded price

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
     $ nohup python3 gateway_api.py > gateway.nohup.log &
     $ nohup python3 node_runner.py > node_1.nohup.log &
     $ nohup python3 node_runner.py -d 2 > node_2.nohup.log &
+
+
+#### Find all process started with nohup python
+    ps -ef | grep "command name"
+    ps -ef | grep "python"
--- sources/finnhub/price_cache.py
+++ sources/finnhub/price_cache.py
@@ -9,16 +9,16 @@ def set_symbol_price(symbol, info):
 
 
 def get_symbol_price(symbol):
-    return {
-        "price": 123.85,
-        "timestamp": 1615724746
-    }
-    # try:
-    #     price = r.get("symbol-price-%s" % symbol.upper())
-    #     if not price:
-    #         return None
-    #     price = json.loads(price)
-    #     return price
-    # except ValueError:
-    #     print("Oops!", sys.exc_info()[0], "occurred.")
-    #     return None
+    # return {
+    #     "price": 123.85,
+    #     "timestamp": 1615724746
+    # }
+    try:
+        price = r.get("symbol-price-%s" % symbol.upper())
+        if not price:
+            return None
+        price = json.loads(price)
+        return price
+    except ValueError:
+        print("Oops!", sys.exc_info()[0], "occurred.")
+        return None
